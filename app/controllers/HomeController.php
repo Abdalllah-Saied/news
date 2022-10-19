c3dd071620ca4cf34dd3a17d6caa1fc522117484
Abdalllah-Saied <abdallasaid201@gmail.com>
2022-10-19 11:40:34 +0200

    no errors in this version

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -7,7 +7,6 @@ use MVC\core\controller;
 class HomeController extends controller {
     public function index(){
         $title='home index';
-
         $this->view("home/index",['title'=>$title]);
     }
     public function add(){
